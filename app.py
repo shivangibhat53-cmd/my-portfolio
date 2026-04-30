965ae2cc45dfb13e144708df9aad88bd093a154b
Shiv <shivangibhat93@gmail.com>
2026-04-30 11:46:27 -0700

    Folder structure created for Modular Flask application ==>Blueprints

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
+import os,uuid
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static\images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 def get_projects():
     conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute("Select* from projects")
     projects = cursor.fetchall()
@@ -16,9 +21,30 @@ def home():
     projects = get_projects()
     return render_template("index.html", projects = projects)
 
-@app.route("/projects")
+@app.route("/projects", methods = ["GET","POST"])
+
 def projects():
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        link = request.form["link"]
+
+        file = request.files.get("image")
+        filename = str(uuid.uuid4()) + file.filename  ##using uuid so matching filenames doesnt cause any issue
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        if file and file.filename !="":
+            file.save(filepath)
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO projects(title,description,link,image)VALUES(?,?,?,?)",(title,description,link,filename))
+        conn.commit()
+        conn.close()
+        return redirect("/projects")
+    
     projects = get_projects()
+    
     return render_template("projects.html",projects = projects)
 
 @app.route("/contact")
--- init_db.py
+++ init_db.py
@@ -4,13 +4,15 @@ conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
 cursor.execute("""CREATE TABLE projects (
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
                title TEXT,
-               description TEXT,
-               link TEXT)""")
+              description TEXT,
+              link TEXT,
+              image TEXT )""")
 
-cursor.execute("INSERT INTO projects(title, description, link) VALUES (?,?,?)",("Portfolio Website", "My Personal portofolio built with Flask","#"))
-cursor.execute("INSERT INTO projects(title, description, link) VALUES (?,?,?)",("Todo App","A simple task manager app", "#"))
-
+#cursor.execute("INSERT INTO projects(title, description, link) VALUES (?,?,?)",("Portfolio Website", "My Personal portofolio built with Flask","#"))
+#cursor.execute("INSERT INTO projects(title, description, link) VALUES (?,?,?)",("Todo App","A simple task manager app", "#"))
+#cursor.execute("UPDATE projects SET image = 'default.jpg' where image is NULL")
+#cursor.execute("DELETE from projects where Id IN (5,6)")
 conn.commit()
 conn.close()
